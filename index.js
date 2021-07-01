15f1b4fc4474728d696d51674d78b207957328b2
Tomáš Hanzel <hanzel.toma@gmail.com>
2021-07-01 20:41:16 +0200

    started registration functionality

--- index.js
+++ index.js
@@ -2,15 +2,45 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"
 
 const database = require('./database.js');
 const express = require("express");
+const bcrypt = require("bcrypt");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.urlencoded({
+    extended: true
+  })
+);
+
+app.use(express.json())
+
 app.get("/api", async (req, res) => {
   User = database.user
-  await User.create({ username: "tester", e_mail: "test@test.com", password: "test", registration_date: new Date().toLocaleString(), createdAt: new Date().toLocaleString(), updatedAt: new Date().toLocaleString() });
+  await User.create({ username: "tester1", e_mail: "test1@test.com", password: "test", registration_date: new Date().toLocaleString(), createdAt: new Date().toLocaleString(), updatedAt: new Date().toLocaleString() });
   res.status(200).send('Everything ok');
+});
+
+app.post("/users/register", async (req, res) => {
+  
+  let { username, e_mail, password, password2} = req.body;
+  let err = []; 
+  
+  if (!username, !e_mail, !password, !password2) {
+    err.push({message: "Please enter all fields"});
+  }
+
+  if (password != password2) {
+    err.push({message: "Passwords do not match"});
+  }
+
+  if (err.length > 0){
+    res.status(400).json({ errors : err });
+  } else {
+    let hashedPw = await bcrypt.hash(password, 10);
+  }
+
+
 });
 
 app.listen(PORT, () => {
